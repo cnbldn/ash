4a0cb7ce1160f132b4e8f22a2c19427787a82a6c
Can Buldan <canbuldan@outlook.com>
2026-02-14 07:56:58 +0300

    descriptive commit message

---
+++ README.md
@@ -0,0 +1,3 @@
+# ash - a shell
+
+Learning project.
--- src/ash.c
+++ src/ash.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int printPrompt() {
-    printf("[ash]%%: ");
-    return 1;
+char *PATH = "/usr/bin";
+
+void printPrompt() {
+    char *promptStr = "[ash]%: ";
+    printf("%s", promptStr);
 }
 
 void getInput() {
@@ -15,9 +19,28 @@ void getInput() {
     free(input);
 }
 
+void executeProgram() {
+    // TODO: check if program is in PATH
+
+    // TODO: handle fork failing
+    if (fork()) {
+        // parent
+        // waits for child to finish
+        // (waits for something in the pipe)
+        // print the output of child
+    } else {
+        // child
+        // execute the program
+    }
+    // fork
+    // program runs, pipes the output back to parent
+    // parent prints output
+}
+
 int main(int argc, char *argv[]) {
     while (1) {
         printPrompt();
         getInput();
+        executeProgram();
     }
 }

90c02b1bd4f28137bdc751639b628c1cb4e6983d
Can Buldan <canbuldan@outlook.com>
2026-03-04 22:47:11 +0300

    very basic and naive program execution added

--- src/ash.c
+++ src/ash.c
@@ -1,4 +1,6 @@
 // Currently loosely following "github.com/tokenrove/build-your-own-shell"
+#include <sys/types.h>
+#include <sys/wait.h>
 #define DEBUG 1
 
 #include <linux/limits.h>
@@ -6,6 +8,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#define PATH "/usr/bin/"
 
 // parses line into an array of strings. Does in fact malloc.
 void parseLine(char *line, char **tokenv, int *tokenc) {
@@ -18,6 +22,13 @@ void parseLine(char *line, char **tokenv, int *tokenc) {
         (*tokenc)++;
         tkn = strtok(NULL, " \n");
     }
+
+#if DEBUG
+    printf("c:%d\n", *tokenc);
+    for (int i = 0; i < *tokenc; i++) {
+        printf("[%d] %s\n", i, tokenv[i]);
+    }
+#endif /* if DEBUG */
 }
 
 int main(int argc, char *argv[]) {
@@ -36,15 +47,26 @@ int main(int argc, char *argv[]) {
         int tokenc;
 
         parseLine(line, tokenv, &tokenc);
-        // exec line
 
-#if DEBUG
-        printf("c:%d\n", tokenc);
-        for (int i = 0; i < tokenc; i++) {
-            printf("[%d] %s\n", i, tokenv[i]);
+        // fork and execute program
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
         }
-#endif /* if DEBUG */
 
+        if (pid == 0) { // child
+            // BUG: i feel like there is some memory leak here
+            char *programPath = strdup(PATH);
+            realloc(programPath,
+                    sizeof(char) * (strlen(programPath) + strlen(tokenv[0])));
+            strcat(programPath, tokenv[0]);
+            execve(programPath, tokenv, NULL);
+        } else { // parent
+            wait(NULL);
+        }
+
+        // free allocated tokens
         for (int i = 0; i < tokenc; i++) {
             free(tokenv[i]);
         }
